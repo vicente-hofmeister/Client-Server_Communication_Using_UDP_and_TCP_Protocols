9d62907f3093e3f06da7118d3d20fbefaa549dc1
vicente-hofmeister <vicente.hofmeister@pucrs.br>
2024-09-09 19:52:43 -0300

    codigo exemplo base

---
+++ src/client/TCPclient.py
@@ -0,0 +1,10 @@
+from socket import *
+serverName = 'servername' #endereco ipv4. localhost = 127.0.0.1
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = input('Input lowercase sentence:')
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print ('From Server:', modifiedSentence.decode())
+clientSocket.close()
