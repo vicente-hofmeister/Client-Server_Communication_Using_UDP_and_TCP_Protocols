840fab2504b95f77898e95a8c82c5dc546cec8e6
vicente-hofmeister <>
2024-09-16 14:51:41 -0300

    app client e udp client refatorados

--- src/AppClient.py
+++ src/AppClient.py
@@ -1,14 +1,16 @@
 import ast
 import os
 import sys
+import threading
 import time
 from client.UDPclient import UDPclient
 from client.TCPclient import TCPclient
 
 serverName = '127.0.0.1'
 serverPort = 12000
+serverAddress = (serverName, serverPort)
 
-global myName, connectionName, sender, receiver, operation, messageType, message, serverAddress
+global client, myName, connectionName, sender, receiver, operation, messageType, message, stop_event 
 
 def clear_terminal():
     if os.name == 'nt':  # Windows
@@ -55,18 +57,72 @@ def decode_message(receivedMessage):
       message = messageContent[1]
       serverAddress = receivedMessage[1]
 
+def manage_response(receivedMessage):
+      decode_message(receivedMessage)
 
-client = None
+      if operation == "response":
+            if messageType == "new_convo" and message == "not_allowed":
+                  print("There is no contact with that name :(\n")
+                  return "denied"
+            elif messageType == "new_convo" and message == "accepted":
+                  print("Connected!")
+                  return "accepted"
+      elif operation == "new_convo":
+            if messageType == "contact":
+                  clear_terminal()
+                  print("{} wants to begin a new contact with you!".format(str(message).capitalize()))
+                  answer = ""
+                  while answer.lower() != 'n' and answer.lower() != 'y':
+                        answer = input("Do you want to connect? (y or n)\n")
+                  if answer.lower() == 'n':
+                        return ("['{}','{}','response',['new_convo','denied']]".format(myName, sender).encode(), serverAddress)
+                  else:
+                        return ("['{}','{}','response',['new_convo','accepted']]".format(myName, sender).encode(), serverAddress)
 
-clear_terminal()
+def sendConnect():
+      global stop_event
+      response = ""
+      while not stop_event.is_set():
+            print("Wish to connect with someone? Write their name!\n")
+            friendName = input()
+            stop_event.set()
+            print("Trying to connect...")
+            clientMessage = "['{}','server','new_convo',['contact','{}']]".format(myName, friendName).encode()
+            client.sendMessage(clientMessage)
+            response = manage_response(client.receiveMessage())
 
-if get_coms_type() == 'udp':
-      client = UDPclient(serverName=serverName, serverPort=serverPort)
-else:
-      client = TCPclient(serverName=serverName, serverPort=serverPort)
+            if type(response) == "str" and response == "denied":
+                  stop_event.clear()
 
-initialize_client()
+def waitMessage():
+      while not stop_event.is_set():
+            receivedMessage = client.receiveMessage()
+            stop_event.set()
+            newMessage = manage_response(receivedMessage)
+            client.sendMessage(newMessage)
+            stop_event.clear()
 
+def start():
+      global client, stop_event
+      client = None
+      stop_event = threading.Event()
+      clear_terminal()
 
+      if get_coms_type() == 'udp':
+            client = UDPclient(serverAddress=serverAddress)
+      else:
+            client = TCPclient(serverAddress=serverAddress)
 
+      initialize_client()
 
+      sendConnectThread = threading.Thread(target=sendConnect)
+      waitMessageThread = threading.Thread(target=waitMessage)
+      # sendMessageThread = threading.Thread(target=sendMessage)
+
+      sendConnectThread.start()
+      waitMessageThread.start()
+
+      # byebye message here
+      # client.closeConnection()
+
+start()
--- src/client/UDPclient.py
+++ src/client/UDPclient.py
@@ -1,38 +1,9 @@
-import ast
 from socket import *
-import threading
-
-from utils import client_utils as utils
 
 class UDPclient:
-      def __init__(self, serverName, serverPort):
-            self.serverAddress = (serverName, serverPort)
+      def __init__(self, serverAddress):
+            self.serverAddress = serverAddress
             self.clientSocket = socket(AF_INET, SOCK_DGRAM)
-            self.stop_event = threading.Event()
-
-      def start(self):
-            
-            def sendConnect():
-                  while not self.stop_event.is_set():
-                        clientMessage, serverAddress = utils.choose_friend()
-                        self.stop_event.set()
-                        self.clientSocket.sendto(clientMessage, serverAddress)
-                        response = utils.manage_response(self.clientSocket.recvfrom(2048))
-
-                        if type(response) == "str" and response == "accepted":
-                              return
-            
-            def waitMessage():
-                  while not self.stop_event.is_set():
-                        receivedMessage = self.clientSocket.recvfrom(2048)
-                        self.stop_event.set()
-                        utils.manage_response(receivedMessage)
-
-            sendConnectThread = threading.Thread(target=sendConnect)
-            receiveConnectThread = threading.Thread(target= waitMessage)
-
-            sendConnectThread.start()
-            receiveConnectThread.start()
 
       def sendMessage(self, message):
             self.clientSocket.sendto(message, self.serverAddress)
