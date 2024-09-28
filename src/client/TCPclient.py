041cdc2e4dd22b0d4d2f02ad4e35322d1fca5fd8
vicente-hofmeister <vicente.hofmeister@pucrs.br>
2024-09-27 21:54:13 -0300

    Documentation for clients and servers files. Not Apps

--- src/client/TCPclient.py
+++ src/client/TCPclient.py
@@ -1,12 +1,38 @@
 from socket import *
 class TCPclient:
+      '''
+      Performs client socket operations through a TCP connection.
+
+      Attributes:
+            serverAddress: A tuple (str, int) representing the IPv4 address and the port of the server.
+            clientSocket: The socket responsible for the client's TCP communications.
+
+      Methods:
+            __init__: Initializes the client's socket.
+            changeServerAddress: Changes the server socket address with which the client communicates.
+            sendMessage: Sends an encoded message from the client to the server.
+            receiveMessage: Receives an encoded message from the server to the client.
+            closeConnection: Closes the client socket.
+      '''
       def __init__(self, serverAddress):
+            '''
+            Initializes the client socket to communicate with the given server address.
+
+            Args:
+                  serverAddress ((str, int)): A tuple with the ipv4 address and port for the initial server socket
+            '''
             self.serverAddress = serverAddress
             self.clientSocket = socket(AF_INET, SOCK_STREAM)
             self.clientSocket.settimeout(0.1)
             self.clientSocket.connect(self.serverAddress)
 
       def changeServerAddress(self, newAddress):
+            '''
+            Changes the server socket address to communicate with the socket dedicated to this client.
+
+            Args:
+                  newAddress ((str, int)): A tuple containing the IPv4 address and port of the server socket.
+            '''
             self.serverAddress = newAddress
             self.clientSocket.close()
             self.clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -14,11 +40,27 @@ class TCPclient:
             self.clientSocket.connect(self.serverAddress)
 
       def sendMessage(self, message):
+            '''
+            Sends an encoded message from the client to the server.
+
+            Args:
+                  message (bytes): The encoded message to be sent from the client.
+            '''
             self.clientSocket.send(message)
       
       def receiveMessage(self):
+            '''
+            Receives an encoded message from the server.
+
+            Returns:
+                  serverMessage (bytes): The encoded message from the server.
+                  serverAddress (tuple): A tuple (str, int) containing the IPv4 address and port of the server socket.
+            '''
             serverMessage = self.clientSocket.recv(2048)
             return serverMessage, self.serverAddress
       
       def closeConnection(self):
+            '''
+            Closes the current socket.
+            '''
             self.clientSocket.close()
--- src/server/UDPserver.py
+++ src/server/UDPserver.py
@@ -1,7 +1,29 @@
 from socket import *
 
 class UDPserver:
+      '''
+      Performs server socket operations through a UDP connection.
+
+      Attributes:
+            serverPort: The port where the server socket runs.
+            client: Stores the name of the client utilizing this server socket.
+            serverSocket: Socket that listens for incoming messages from the client.
+            
+      Methods:
+            __init__: Initializes the server socket.
+            receiveMessage: Receives a message from the client.
+            sendMessage: Sends a message to the client.
+            getClient: Returns the name of the connected client.
+            closeSocket: Closes the server socket.
+      '''
       def __init__(self, serverPort, client):
+            '''
+            Initializes the server socket with its port and client name.
+
+            Args:
+                  serverPort (int): The port number that this server socket uses.
+                  client (str): The name of the client connected through this server socket.
+            '''    
             self.serverPort = serverPort
             self.serverSocket = socket(AF_INET, SOCK_DGRAM)
             self.serverSocket.bind(('', self.serverPort))
@@ -9,14 +31,39 @@ class UDPserver:
             self.client = client
 
       def receiveMessage(self):
+            '''
+            
+            Receives a message from the client. If there is not a client connected, waits to connect.
+
+            Returns:
+                  bytes: Encoded client message
+
+            '''
             clientMessage =  self.serverSocket.recvfrom(2048)
             return clientMessage
       
       def sendMessage(self, message, address):
+            '''
+            Receives a message from the client. If there is no client connected, waits for a connection.
+
+            Returns:
+                  bytes: The encoded message from the client.
+            '''
+
+
             self.serverSocket.sendto(message, address)
 
       def getClient(self):
+            '''
+            Gets the client of this server socket.
+
+            Returns:
+                  str: The name of the client connected to this server socket.
+            '''
             return self.client
       
       def closeSocket(self):
+            '''
+            Closes this server socket.
+            '''
             self.serverSocket.close()
